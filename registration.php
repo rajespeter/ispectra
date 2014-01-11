f7bc4e11cfe55d3ed5222187aa4b770d2ea57649
rajeshpeter <rajeshpeter@gmail.com>
2014-01-10 17:23:48 -0800

    clean up

--- registration.php
+++ registration.php
@@ -30,21 +30,12 @@ require_once("./includes/initialize.php"); ?>
                 <h3 align="center" >Information </h3>
                 
                 
-<<<<<<< HEAD
                 <p> Registration Fees:  <br /> 
                     refreshments and materials  <br />
                     Adult On-Line Registration fees:<br />
                     Prior to TBD: &nbsp; TBD per person <br/>
                     TBD<br/>
                     TBD and<br/>
-=======
-                <p> Registration Fees:   Includes SATURDAY lunch, <br /> 
-                    refreshments and materials  <br />
-                    Adult On-Line Registration fees:<br />
-                    Prior to August 31: &nbsp; $50 per person <br/>
-                    September 1 - October 17: $60<br/>
-                    October 18 and<br/>
->>>>>>> 9b688efcfd7a97f3cc9e4c8f62e16aaff81bb5c0
                     Adult Registration At the door:    &nbsp; $99 per person<br/>
                 </p>
                 
@@ -59,35 +50,22 @@ require_once("./includes/initialize.php"); ?>
                  <hr />
                  <p>You can Register additional people with you as your register on-line. <br />
                  If you Register an additional 4 or more adults at the same time  <br />
-<<<<<<< HEAD
                  you save TBD per adult automaticly </p>        
-=======
-                 you save $20 per adult automaticly </p>        
->>>>>>> 9b688efcfd7a97f3cc9e4c8f62e16aaff81bb5c0
           
               
                 <hr />
                
-<<<<<<< HEAD
                 <p> When you Register you can also register for Ethnic Training conducted by XXXX prior<br />the iSpectra 2014 workshops 
-=======
-                <p> When you Register you can add downloads of all of<br />the ENGAGE 2013 workshops 
->>>>>>> 9b688efcfd7a97f3cc9e4c8f62e16aaff81bb5c0
                    as add-on <br /> - $30 for set per attendee</p>
                    <hr />
        
                <br/>
                <p>For questions regarding registration contact <br/>
-<<<<<<< HEAD
                     By E-mail: somone@nowhere.org <br/>
                 By Phone:   xxx-xxx-xx </p>
-=======
-                    By E-mail: John.Dupree@Perspectives.org <br/>
-                By Phone:   (209) 848-2262 </p>
->>>>>>> 9b688efcfd7a97f3cc9e4c8f62e16aaff81bb5c0
               
                
-                
+                  
             </div>
 
            
@@ -97,17 +75,10 @@ require_once("./includes/initialize.php"); ?>
             
              <p class="clear"> </p> 
              
-<<<<<<< HEAD
              <!--h2 align="center">Special Registration for Host Church attendees</h2>
              
              <p align="center" ><a href='./PayPalCart/html/processGroup.php?id=HOST' target="">Host Church</a>   </p>
                 <p><br /></p-->  
-=======
-             <h2 align="center">Special Registration for Host Church attendees</h2>
-             
-             <p align="center" ><a href='./PayPalCart/html/processGroup.php?id=HOST' target="">Host Church</a>   </p>
-                <p><br /></p>  
->>>>>>> 9b688efcfd7a97f3cc9e4c8f62e16aaff81bb5c0
          </div> <!-- content -->
 </div> <!-- outerDiv -->
 
