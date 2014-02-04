fbf4a1a9e380b06f2cfbf39715d27eea39c44ac3
rajeshpeter <rajeshpeter@gmail.com>
2014-02-03 17:00:52 -0800

    ClearCart bug
    sesión clean up bug
    files affected clearCart.php

--- PayPalCart/html/clearCart.php
+++ PayPalCart/html/clearCart.php
@@ -1,20 +1,19 @@
 <?php 
+ob_start();
 require_once '../functions/PP_functions.php';
-ob_start();  
+  
 
 $shopping_cart = get_shopping_cart();
 
 if (isset($_REQUEST['clear'])) {
     $shopping_cart->EmptyCart();
-    echo '<h2>Shopping Cart Emptied!</h2>';
 	set_shopping_cart($shopping_cart);
 	redirect_to("../../registration.php?clear=1");  
 }
 
 if (isset($_REQUEST['complete'])) {
     $shopping_cart->EmptyCart();
-    echo '<h2>Transaction Completed!</h2>';
-	set_shopping_cart($shopping_cart);
+  	set_shopping_cart($shopping_cart);
 	
 
 redirect_to("../../registration.php?complete=1");  
